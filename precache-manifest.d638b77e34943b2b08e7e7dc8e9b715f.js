307739132cc86577e5f095a3f3a80e8af3d3c0a2
Ibrahim Animashaun <iaanimashaun@gmail.com>
2022-09-10 06:04:25 +0100

    Updates

---
+++ precache-manifest.d638b77e34943b2b08e7e7dc8e9b715f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b757fc0dce4959560f4a71c5a1465f",
+    "url": "/happybirthday/index.html"
+  },
+  {
+    "revision": "677284ead2540c444e71",
+    "url": "/happybirthday/static/css/main.31b6137c.chunk.css"
+  },
+  {
+    "revision": "39e49d183e5f61f7a5c0",
+    "url": "/happybirthday/static/js/2.08f93be2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/happybirthday/static/js/2.08f93be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "677284ead2540c444e71",
+    "url": "/happybirthday/static/js/main.dd597b25.chunk.js"
+  },
+  {
+    "revision": "a378511c95b3c77cd49c",
+    "url": "/happybirthday/static/js/runtime-main.27748ea3.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/happybirthday/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
